def252043f73d6a7a947ea66c8a5897cc53999cf
Juan Carlos De la O <carlosdelao1234@gmail.com>
2020-06-16 00:56:55 -0500

    Updates for forecast and about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,23 @@ const forecast = (longitude, latitude, callback) =>{
     const url = "http://api.weatherstack.com/current?access_key=bf458a0d3770e8ac949233ff762646a7"+
     "&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude);
     request( { url, json:true }, (error, { body }) =>{
+        const weather_descriptions = body.current.weather_descriptions[0];
+        const temperature = body.current.temperature;
+        const feelslike = body.current.feelslike;
+        const time = body.location.localtime;
+        const humidity = body.current.humidity;
+        const visibility = body.current.visibility;
+
         if(error){
             callback("Can not connected with API", undefined);
         }else if(body.error){
             callback(body.error.info, undefined)
         }else{
             callback(undefined,{
-                message: " " + body.current.weather_descriptions[0] + ". It's currently:" + body.current.temperature + " degrees out and It's feels like " + body.current.feelslike + " degrees out"
+                message: "Today at " + time + " The weather is: " + weather_descriptions + ", and currently: " 
+                        + temperature + " degrees out with " + humidity + " humidity and "
+                        + visibility + " visibility, but It's feels like " + feelslike + " degrees out"
+
             });  
         }
     });
